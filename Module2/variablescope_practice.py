14ea4f905f61897db750b5cea6f7666a7a538fef
takhancode <talhadua0@gmail.com>
2026-06-27 20:17:17 +0500

    Update Module2 exercises

---
+++ Module2/variablescope_practice.py
@@ -0,0 +1,12 @@
+""" Write a program that:
+
+Creates a global variable name = "Ali".
+Creates a function show_name().
+Prints the global variable inside the function.
+Calls the function. """
+
+name = "Ali"
+
+def show_name():
+    print(name)
+show_name()
